df5295807a0701943ee85c7077b0e5f6b6c34e96
MiryamRM <mirytaramosmartinez@gmail.com>
2020-08-18 19:40:23 +0200

    dia18 agosto
    cambios

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -3,12 +3,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
 
+        public function __construct(){
+                parent::__construct();
+        }
+
 	public function index(){
-        $view['titulo'] = "Bienvenido al master en CodeIgniter";
-        $view['header'] = $this->load->view('template/header',null,TRUE) ;
-        $view['navega'] = $this->load->view('template/nav',null,TRUE) ;
-        $view['footer'] = $this->load->view('template/footer',null,TRUE)  ;
-        $view['body'] = $this->load->view('dashboard',null,TRUE) ;
-        $this->parser->parse('home',$view);
+                $view['titulo'] = "Bienvenido al master en CodeIgniter";
+                $view['header'] = $this->load->view('template/header',null,TRUE) ;
+                $view['navega'] = $this->load->view('template/nav',null,TRUE) ;
+                $view['footer'] = $this->load->view('template/footer',null,TRUE)  ;
+                $view['body'] = $this->load->view('dashboard',null,TRUE) ;
+                $this->parser->parse('home',$view);
+
 	}
 }
